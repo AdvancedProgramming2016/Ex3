d2fa1ae6961b7b07507ee6654823c6193477f3c3
bitrono <bitrono@yahoo.com>
2016-12-16 00:01:13 +0200

    changed the coefficient

--- src/LuxuryVehicle.cpp
+++ src/LuxuryVehicle.cpp
@@ -5,5 +5,5 @@
 LuxuryVehicle::LuxuryVehicle(unsigned int vehicleId,
                              char carManufacturer, char color)
         : Vehicle(vehicleId, carManufacturer, color) {
-    tariff = 2;
+    coefficient = 2;
 }
