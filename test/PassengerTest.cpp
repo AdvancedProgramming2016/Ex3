da27a29fcef57cf8cbb183e4eb4c7cce1123fb55
bitrono <bitrono@yahoo.com>
2016-12-17 11:22:34 +0200

    Fixed all the tests from Ex. 2 and Removed the taxi property from Trip class

--- test/PassengerTest.cpp
+++ test/PassengerTest.cpp
@@ -6,12 +6,6 @@ class PassengerTest : public ::testing::Test {
 protected:
     Passenger passenger;
 
-    virtual void SetUp() {
-    }
-
-    virtual void TearDown() {
-    }
-
 public:
     PassengerTest(): passenger(Point(0,0), Point(2,2)){}
 
@@ -26,7 +20,8 @@ TEST_F(PassengerTest, checkPassenger){
 
     value = passenger.generateSatisfaction();
 
-    ASSERT_GT(value, 5); // check that the satisfaction isn't higher than 5.
-    ASSERT_LT(value, 1); // check that the satisfaction isn't lower than 1.
+    ASSERT_LE(value, 5); // check that the satisfaction isn't higher than 5.
+    ASSERT_GE(value, 1); // check that the satisfaction isn't lower than 1.
+
 }
 
