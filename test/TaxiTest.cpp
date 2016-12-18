32e80f2715596f73142c8847d58f1225f73e49bf
redperov <redperov@gmail>
2016-12-18 15:15:36 +0200

    Updated TaxiTest to pass test

--- test/TaxiTest.cpp
+++ test/TaxiTest.cpp
@@ -2,30 +2,27 @@
 #include "../src/Taxi.h"
 #include "../src/StandardVehicle.h"
 
-class TaxiTest : public ::testing::Test {
 
-};
-
-TEST_F(TaxiTest, basicTest) {
+TEST(TaxiTest, basicTest) {
 
     // Init variables
     unsigned initSatisfaction = 0;
-    unsigned newSatisfaction = 0;
-    Driver *driver = new Driver(1, 40, 'M', 20, 1);
-    Vehicle *vehicle = new StandardVehicle(1, 'S', 'P');
-    Point start(0, 0);
-    Point end(2, 3);
+    unsigned newSatisfaction  = 0;
+    Driver   *driver          = new Driver(1, 40, 'M', 20, 1);
+    Vehicle  *vehicle         = new StandardVehicle(1, 'S', 'P');
+    Point    start(0, 0);
+    Point    end(2, 3);
 
     Trip *trip = new Trip(1, start, end, 3, 20);
     Taxi *taxi = new Taxi(driver, vehicle, Point(0, 0));
 
-    // Not implemented yet
-    //taxi->endTrip();
-    //newSatisfaction = taxi->getDriver()->getAvgSatisfaction();
+    taxi->endTrip();
+    newSatisfaction = taxi->getDriver()->getAvgSatisfaction();
 
-    // ASSERT_TRUE(taxi->getTrip() == 0);                 //check that the trip was set to NULL
+    ASSERT_TRUE(taxi->getTrip() ==
+                0);                 //check that the trip was set to NULL
     //EXPECT_NE(initSatisfaction,
-    //        newSatisfaction); //check that the average satisfaction was changed, not mandatory
+      //        newSatisfaction); //check that the average satisfaction was changed, not mandatory
 
     //Delete Variables
     delete taxi;
